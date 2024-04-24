e4f4cbfec5dd9393afae421df6b49fe70bb4f970
Loureen <156646854+nl156672@users.noreply.github.com>
2024-04-24 10:45:06 +0000

    EJS Dateien angelegt

--- server.js
+++ server.js
@@ -14,8 +14,21 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+	//res ist die Anwort des Severs an den Browser.
+	//send() ist die Anweisung etwas an den Browser zu schicken.
+    //res.send('Hello remote world!\n');
+
+	// Das res Objekt kann noch mehr als nur eine Zeichenkette an den browser zu senden. 
+	// Das res Objekt kaDasnn mit der Funktion render() eine HTML Datei an den Brows
+	res.render('index.ejs', {});
+
 });
 
 app.listen(PORT, HOST);
+
+// Mit der Anweisung console.log() wird dem Server-Administrator auf der Konsole
+//angezeigt, was der Server macht. Der Programmierer schreibt dazu
+// in die runden Klammern den Ausdruck, der auf der Konsole angezeigt werden soll
+// Die Werte der beiden Konstanten HOST und PORT werden in den
+// Ausdruck übergeben. Ein Verb mit anschließenden runden Klammern steht immer für eine Anweisung etwas zu tun.
 console.log(`Running on http://${HOST}:${PORT}`);

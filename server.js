80a94eceed88aadae69161fb5a62c9de9caebe50
Loureen <156646854+nl156672@users.noreply.github.com>
2024-11-11 08:08:40 +0000

    BorkenBank: nmicht geklaptt

--- server.js
+++ server.js
@@ -4,46 +4,49 @@
  *--------------------------------------------------------------------------------------------*/
 
 'use strict';
-
+ 
 const express = require('express');
-
+ 
 // Constants
-const PORT = 3001;
+const PORT = 3002;
 const HOST = '0.0.0.0';
-
+ 
 // App
 const app = express();
-// Es wird der App bekanntgegeben, wo die Styles ztu finden sind
-
+ 
+// Es wird der App bekanntgegeben, wo die styles zu finden sind
 app.use(express.static('public'))
-
-
-
-
+ 
+ 
+ 
+ 
+ 
 app.get('/', (req, res) => {
-	//res ist die Anwort des Severs an den Browser.
-	//send() ist die Anweisung etwas an den Browser zu schicken.
+ 
+   
+    //res ist die Antwort des Servers an den Browser
+    //send() ist die Anweisung etwas an den Browser zu schicken
     //res.send('Hello remote world!\n');
-
-	// Das res Objekt kann noch mehr als nur eine Zeichenkette an den browser zu senden. 
-	// Das res Objekt kaDasnn mit der Funktion render() eine HTML Datei an den Brows
-	res.render('index.ejs', {});
-
+ 
+    //Das res-Objekt kann noch mehr als nur ein Zeichenkette an den Browser zu senden.
+    //Das res-Objekt kann mit der Funktion render() eine HTML-Datei an den Browser senden
+    res.render('index.ejs',{});
 });
-
+ 
 app.listen(PORT, HOST);
-
+ 
 // Mit der Anweisung console.log() wird dem Server-Administrator auf der Konsole
 //angezeigt, was der Server macht. Der Programmierer schreibt dazu
 // in die runden Klammern den Ausdruck, der auf der Konsole angezeigt werden soll
 // Die Werte der beiden Konstanten HOST und PORT werden in den
 // Ausdruck übergeben. Ein Verb mit anschließenden runden Klammern steht immer für eine Anweisung etwas zu tun.
 console.log(`Running on http://${HOST}:${PORT}`);
-
-// require('./Uebungen/01-grundlagen.js');
-// require('./Uebungen/02-boolean.js');
-// require('./Uebungen/03-objekte.js');
-// require('./Klausuren/ KlausurTest20240930.js')
-// require('./Uebungen/04-funktionen.js');
-
-
+ 
+//require('./uebungen/01-grundlagen.js');
+ 
+//require('./uebungen/03-objekte.js');
+//require('./Klausuren/klausurtest20240930.js');
+//require('./uebungen/04-funktionen')
+ 
+ 
+ 
